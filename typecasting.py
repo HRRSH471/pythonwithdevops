ab5a99a7441b91c18131b64c1e0e86f2c7817b32
HRRSH471 <hariomharish50gmail.com>
2026-05-01 17:46:09 +0530

    new file updated

--- typecasting.py
+++ typecasting.py
@@ -1,3 +1,11 @@
-var1=float(1000)
+"""var1=float(1000)
 print(type(var1))
-print(var1)
+print(var1)"""""
+
+num1=50
+num2=20
+print(type(num1),type(num2))
+res=str(num1)+str(num2)
+print(res)
+print(type(res))
+
