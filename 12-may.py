dca95b9f0092bba3ced570030d6a5144c2292e77
Hariom <Hariomharish50@gmail.com>
2026-05-13 12:10:48 +0530

    new file updated

--- 12-may.py
+++ 12-may.py
@@ -32,8 +32,36 @@
 # print("Number of consonants in the string:",c_count)
 
 # WAp to print your name in regverse formate
-name = input("Enter your name: ")
-reverse_name = ""
-for i in name:
-    reverse_name = i + reverse_name
-print("Your name in reverse format is:", reverse_name, end=" ")
+# name = input("Enter your name: ")
+# reverse_name = ""
+# for i in name:
+#     reverse_name = i + reverse_name
+# print("Your name in reverse format is:", reverse_name, end=" ")
+
+# WAP to sum of the indices of ta string: "python:"
+str1 = "python"
+Count_indices = 0
+for i in range(len(str1)):
+    Count_indices += i
+    print("Sum of indices:", Count_indices)
+
+# # Wap to print the factorial from 1 to 8
+# for num in range(1, 9):
+#     factorial = 1
+#     for i in range(1, num + 1):
+#         factorial *= i
+#     print(f"Factorial of {num} is: {factorial}")
+
+# Wap to print the only prime numbers from 1 t0 15
+# for num in range(1, 16):
+#     if num > 1:  # prime numbers are greater than 1
+#         for i in range(2, num):
+#             if num % i == 0:
+#                 break
+#         else:
+#             print(num)
+
+# Wap to print the only odd numbers from 1 t0 15
+# for num in range(1, 16):
+#     if num % 2 != 0:  # odd numbers are not divisible by 2                
+
