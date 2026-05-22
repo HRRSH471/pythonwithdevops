b2e765782a152a72dc0348719563f8a76700ea57
Hariom <Hariomharish50@gmail.com>
2026-05-22 12:05:48 +0530

    question 5 done

--- function.py
+++ function.py
@@ -1,1 +1,38 @@
- 
+ # 1. Calculate area of a triangle
+
+# def area_triangle(base,height):
+#         area = 1/2*base*height
+#         return area
+# res = area_triangle(int(input("Enter base value :")),int(input("Enter the height :")))
+# print(res)
+
+# 2. Waf to Calculate perimeter of a square
+# def square_perimeter(a):
+#         perimeter=4*a
+#         return perimeter
+# res = square_perimeter(int(input("Enter the side of square :")))
+# print("Are of Square",res)
+
+#  3.Calculate diameter of a circle
+
+# def diameter(radius):
+#     diameter=2*radius
+#     return diameter
+# res = diameter(int(input("Enter the Radius of Circle :")))
+# print("Diametere of Circle Is :",res)
+
+# 4.Calculate volume of a cube
+# def volume(a):
+#     volum=a**3
+#     return volum
+# res = volume(int(input("Enter the side of cube : ")))
+# print ("Volume of Cube is :",res)
+
+# 5.Calculate surface area of a cuboid
+# def cuboid_surface_area(l,b,h,):
+#     cuboid_surface_area=2*((l*b)+(b*h)+(h*l))
+#     return cuboid_surface_area
+# res = cuboid_surface_area(int(input("Enter the lenght of cuboid : ")),int(input("Enter the breath of cuboid : ")),int(input("Enter the height of cuboid : ")))
+# print("Surface Area of Cuboid is : ",res)
+
+
